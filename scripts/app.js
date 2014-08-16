f507a31344d268a04c2f2a88fc4059d5292af222
Adroaldo de Andrade <adroaldof@gmail.com>
2014-08-16 16:12:21 -0300

    Change folder name to adapt ‘yo angular’ file generation

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,9 @@ mainApp.config(function ($interpolateProvider) {
 
 // Define app routes
 mainApp.config(function ($routeProvider) {
-  $routeProvider.when('/angulife', {templateUrl: 'pages/angulife.html', controller: 'AngulifeCtrl'});
-  $routeProvider.when('/buscarissues', {templateUrl: 'pages/fetchissues.html'});
-  $routeProvider.when('/home', {templateUrl: 'pages/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/angulife', {templateUrl: 'views/angulife.html', controller: 'AngulifeCtrl'});
+  $routeProvider.when('/buscarissues', {templateUrl: 'views/fetchissues.html'});
+  $routeProvider.when('/home', {templateUrl: 'views/home.html', controller: 'HomeCtrl'});
   $routeProvider.otherwise({redirectTo: '/home'});
 });
 
