15c4dbed96f102a5ab2184d05939db4da47d8707
Adroaldo de Andrade <adroaldof@gmail.com>
2014-09-01 11:50:37 -0300

    Remove unused mocks on tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,7 +22,6 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-route/angular-route.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
